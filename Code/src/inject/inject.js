5ecbe84f5173bbc37860a480dbba6a91e9d94617
b-ploetzeneder <b.ploetzeneder@gmail.com>
2014-01-10 10:33:16 +0100

    rewrote so that it works with options
    + killed multiple js files for multiple languages

--- Code/src/inject/inject.js
+++ Code/src/inject/inject.js
@@ -1,22 +1,35 @@
+// injects a language link instead of discussion link on Wikipedia
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
-
-		var english_link = document.getElementsByClassName("interlanguage-link interwiki-en");
-  
-		console.log(english_link[0].innerHTML); 
-		
-		var discussion_link = document.getElementById("ca-talk");
-		// Just to test if you got the lelement
-  		console.log(discussion_link.innerHTML);  
 	
-		
-    	var newtab = document.createElement('li');
-		newtab.innerHTML="<span>"+english_link[0].innerHTML+"</span>";
-		discussion_link.parentNode.replaceChild(newtab, discussion_link);
-      //  discussion_link.parentElement.insertAdjacentElement('afterEnd',newtab); 
+		clearInterval(readyStateCheckInterval);
+	
+		var languagelink;
 
+		//try to get chrome storage data for language 1
+		chrome.storage.sync.get("l1", function(data) { 
+		 	var key=data.l1;
+			//find the element that contains string describing data 1
+			languagelink = document.getElementsByClassName(key);
+			 
+			if (!languagelink)
+			{
+			  console.log("Didn't find Language 1, try again");
+			  chrome.storage.sync.get("l2", function(data) { 
+				key=data.l2;
+				languagelink = document.getElementsByClassName(key);
+				});
+			}
+		    //get the discussion element
+			var discussion_link = document.getElementById("ca-talk");
+		 	
+			//replace it with newly generated tab
+			var newtab = document.createElement('li');
+			newtab.innerHTML="<span>"+languagelink[0].innerHTML+"</span>";
+			discussion_link.parentNode.replaceChild(newtab, discussion_link);
+			 
+		});
 	}
 	}, 10);
 });
